48643e299808d929c8f294a7ef6a744c6e2f2b15
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-16 16:35:09 +0530

    Professor's Bald Dialogue created

---
+++ dialouges/professor_bald_dialogue.py
@@ -0,0 +1,11 @@
+PROFESSOR_BALD_DIALOGUES="""Welcome, young engineer, to the world of Kubernetes.
+I am Professor Bald Uncle.
+Though many simply call me - The Kubernetes Prof.
+This vast world runs on Clusters and Containers.
+Some fear it.
+Some attempt to tame it.
+And a rare few seek to master it.
+As for me?
+I have devoted my life to studying Kubernetes.
+And today onwards, your journey to the world of Kubernetes begins.
+"""
