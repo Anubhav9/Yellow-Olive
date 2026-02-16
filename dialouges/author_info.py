8552adb4ecd4a758c86207b41780ad8f6d6f5eee
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-16 16:34:00 +0530

    Author Info File created

---
+++ dialouges/author_info.py
@@ -0,0 +1,4 @@
+AUTHOR_INFO="""The game has been designed and developed by Anubhav Sanyal.
+You can reach out to Anubhav on his Github: Anubhav9
+Anubhav is also available on Linkedin: @anubhav-sanyal
+"""
