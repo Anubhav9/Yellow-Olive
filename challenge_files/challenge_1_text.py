22a3d0a34299c0aa2a1986836cf7ad40ef31ca34
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-16 16:36:27 +0530

    Create challenge_1_text.py

---
+++ challenge_files/challenge_1_text.py
@@ -0,0 +1,9 @@
+CHALLENGE_1_TEXT = """Electromon is usually shy.
+But today, something is different.
+He refuses to step out of his Pokepod.
+As his trainer — and his partner in crime — it’s your responsibility to discover what’s wrong.
+Why won’t Electromon emerge?
+What is holding him back?
+The cluster awaits your decision.
+Can you fix this?
+"""
