7f5311d8c437b664258be2c1eb4c80adc9c4d831
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-16 16:34:34 +0530

    Game Reference Dialogue created

---
+++ dialouges/game_reference_dialogue.py
@@ -0,0 +1,8 @@
+GAME_REFERENCE_DIALOGUE="""Listen carefully, young engineer...
+Posemons are the ones who fight the battles.
+( They are called Containers elsewhere )
+PokePods carry Posemons safely.
+( Other often refer to them as Pods )
+Towns define your territory. 
+( Think of Namespaces in Kubernetes ) 
+"""
