2798fb7ed10f0318be7e28a4618a65b98c8dcfbe
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-16 16:35:39 +0530

    Create professor_bald_dialogue_name_input_screen.py

---
+++ dialouges/professor_bald_dialogue_name_input_screen.py
@@ -0,0 +1,9 @@
+PROFESSOR_BALD_DIALOGUE_NAME_INPUT_SCREEN="""Ah, {user_name}! A fine name for a young future Kubernetes master.
+Your very own Kubernetes legend is about to unfold. A world of dreams and adventure with Kubernetes awaits you
+But you are late to the party.
+I only have 1 Posemon left for you now.
+I am giving you my Electromon as your starter buddy.
+It is usually shy but sometimes can have an electrifying personality.
+Though it is usually shy, it hates staying in it's PokePod.
+Hope you are excited to meet him!
+"""
