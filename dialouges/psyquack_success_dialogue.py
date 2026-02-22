ccde6b90e2d020d7adea06f9ff1e98cd0da29849
Anubhav Sanyal <princebest3@rediffmail.com>
2026-02-22 17:30:46 +0530

    Added new PsyQuack Success Screen Dialogue

---
+++ dialouges/psyquack_success_dialogue.py
@@ -0,0 +1,11 @@
+PSYQUACK_SUCCESS_DIALOGUES = """PsyQuack’s eyes light up!
+It worked! It actually worked!
+He jumps with joy, flapping his tiny wings.
+The cluster is stable... the Pokepod is healthy again!
+Trainer, your debugging skills are growing stronger.
+Mission Complete.
+What would you like to do next?
+• Type `psyquack next`  — Continue your journey  
+PsyQuack is very proud of you!
+This is how Kubernetes Masters are made.
+"""
