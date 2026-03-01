8bcfff204f31b653b27f41151770201cf85c7435
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-01 08:00:10 +0530

    changes to accommodate the new structure

--- screens/psy_quack_failure_screen.py
+++ screens/psy_quack_failure_screen.py
@@ -1,11 +1,11 @@
-import asyncio
-import pygame
-from textual import on
-from textual.widgets import Static, RichLog, Input, Label
-
 from utils import general_utils
 from dialouges import psyquack_failure_screen_dialogue
-
+import global_constants
+from media import background_music_utility
+from screen_prompts.screen_psyquack_failure_screen import screen_prompts
+import asyncio
+from textual import on
+from textual.widgets import Static, RichLog, Input, Label
 
 class PsyQuackFailureScreen(Static):
     can_focus = True
@@ -25,35 +25,23 @@ class PsyQuackFailureScreen(Static):
         log = self.query_one("#failure-log", RichLog)
         prompt = self.query_one("#failure-prompt", Label)
         inp = self.query_one("#failure-input", Input)
-
-        pygame.mixer.init()
-        pygame.mixer.music.load("psyquack_voice.mp3")
-        pygame.mixer.music.play(loops=0)
-
-        color = "#D4AF37"
+        background_music_utility.start_background_music(f"{global_constants.MUSIC_MEDIA_PATH}/psyquack_voice.mp3")
         dialogues = psyquack_failure_screen_dialogue.PSYQUACK_FAILURE_DIALOGUES
-
         for line in dialogues.split("\n"):
-            log.write(f"[bold {color}]{line}[/]\n")
+            log.write(f"[bold {global_constants.GLOBAL_DIALOGUE_COLOR}]{line}[/]\n")
             await asyncio.sleep(1)
-
-        log.write(general_utils.convert_to_ascii("psyquack.png"))
-
-        prompt.update(f"[bold {color}]Young Engineer, what would you like to do next?[/]")
+        log.write(general_utils.convert_to_ascii(f"{global_constants.IMAGE_MEDIA_PATH}/psyquack.png"))
+        prompt.update(screen_prompts.BACK_TO_PREVIOUS_SCREEN)
         inp.focus()
 
     @on(Input.Submitted, selector="#failure-input")
     async def on_input(self, event: Input.Submitted) -> None:
         command = (event.value or "").strip().lower()
-
-        pygame.mixer.music.stop()
-        pygame.mixer.music.unload()
-
+        background_music_utility.stop_background_music()
         if command == "psyquack back":
             ChallengeScreen = general_utils.load_challenge(self.challenge_id)
             container = self.parent
             # remove all children safely
             for child in list(container.children):
                 await child.remove()
-
             await container.mount(ChallengeScreen())
