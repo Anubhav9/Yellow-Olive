dd25333990511139bbe7e5f817d62df46441fcb0
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-08 11:47:34 +0530

    Help Screen added

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from screens import professor_bald_intro
 from dialouges import professor_bald_dialogue
 from screens.author_info import AuthorInfo
 from screens.professor_bald_intro import ProfessorBaldIntro
+from screens.help_screen import HelpScreen
 
 
 class ProjectOlive(App):
@@ -32,9 +33,16 @@ class ProjectOlive(App):
         await game_area.mount(professor_bald_intro)
         self.run_worker(professor_bald_intro.render_professor_bald_intro(professor_bald_dialogue.PROFESSOR_BALD_DIALOGUES,"#D4AF37"))
 
+    @on(Button.Pressed, "#help")
+    async def button_press_help(self, event=Button.Pressed):
+        game_area = self.query_one("#game-flow")
+        for child in list(game_area.children):
+            await child.remove()
+        help_screen = HelpScreen()
+        await game_area.mount(help_screen)
+
 
 
 if __name__ == "__main__":
     app=ProjectOlive()
     app.run()
-
