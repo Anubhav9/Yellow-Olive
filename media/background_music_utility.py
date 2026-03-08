5c45d364311887fc87366f6819b5df58f5630b91
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-08 16:02:04 +0530

    bug resolved - check for background driver

--- media/background_music_utility.py
+++ media/background_music_utility.py
@@ -1,20 +1,23 @@
 import pygame
 
-def start_background_music(background_music_path,number_of_loops=0):
+def start_background_music(background_music_path, number_of_loops=0):
     """
     Utility function to start the background music in the game
-    :param background_music_path: Path to the background music
-    :param number_of_loops: Number of times, the background music needs to be played
-    :return: None
     """
-    pygame.mixer.init()
-    pygame.mixer.music.load(background_music_path)
-    pygame.mixer.music.play(loops=number_of_loops)
+    try:
+        pygame.mixer.init()
+        pygame.mixer.music.load(background_music_path)
+        pygame.mixer.music.play(loops=number_of_loops)
+    except pygame.error:
+        print("Audio device not available. Skipping background music.")
+
 
 def stop_background_music():
     """
     Stops the running background music
-    :return: None
     """
-    pygame.mixer.music.stop()
-    pygame.mixer.music.unload()
+    try:
+        pygame.mixer.music.stop()
+        pygame.mixer.music.unload()
+    except pygame.error:
+        pass
