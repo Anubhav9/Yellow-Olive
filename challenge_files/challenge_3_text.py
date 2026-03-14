363753c5a5690aa88bacf511c66cba6f497d40ea
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-14 14:44:42 +0530

    challenge 3 created

---
+++ challenge_files/challenge_3_text.py
@@ -0,0 +1,19 @@
+CHALLENGE_3_TEXT = """You are happy.
+Electromon is happy.
+The two of you take a cheerful walk
+through one of the nearby towns in the cluster.
+Every town has its own trainers,
+its own Posemons,
+and its own little stories.
+But today is not just about walking.
+You want to begin Electromon's training.
+As an electric Posemon, Electromon is capable of unleashing
+a powerful shock in battle.
+But Posemons do not reveal their power
+unless their trainer gives the right command.
+Can you convince Electromon to demonstrate his shocking capability?
+
+Objective:
+Configure the container command so that it prints
+Electromon show your power
+"""
