9ea9d5b3ea96a8eae64157966273bd9f952cb55f
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-14 14:45:33 +0530

    Challenge #5 created

---
+++ challenge_files/challenge_5_text.py
@@ -0,0 +1,15 @@
+CHALLENGE_5_TEXT = """Electromon has grown stronger.
+The sparks around him feel sharper. His movements feel faster.
+Training has gone well, but battle requires preparation.
+Experienced trainers know that before a Posemon enters battle,
+a special instruction must be placed inside the Pokepod.
+Only then will the Posemon
+switch into battle mode.
+Electromon is waiting patiently.
+Place the correct instruction inside Electromon's Pokepod
+so he is ready to fight.
+
+Objective:
+Configure env variable
+BATTLE_MODE=ON
+"""
