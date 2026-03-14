b92b55d72fc7190aa7d2413045457f398ba78fb0
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-14 14:48:47 +0530

    Updated challenge_2 file

---
+++ screens/challenge_2.py
@@ -0,0 +1,7 @@
+from challenge_files import challenge_2_text
+from screens.base_challenge_screen import BaseChallengeScreen
+
+
+class Challenge2(BasxeChallengeScreen):
+    challenge_id = "2"
+    challenge_text = challenge_2_text.CHALLENGE_2_TEXT
