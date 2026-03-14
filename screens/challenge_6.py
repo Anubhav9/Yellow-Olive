f1e15069b6e109b95c3ccb264b39694be68553ef
Anubhav Sanyal <princebest3@rediffmail.com>
2026-03-14 14:50:49 +0530

    created challenge#6 file

---
+++ screens/challenge_6.py
@@ -0,0 +1,7 @@
+from challenge_files import challenge_6_text
+from screens.base_challenge_screen import BaseChallengeScreen
+
+
+class Challenge6(BaseChallengeScreen):
+    challenge_id = "6"
+    challenge_text = challenge_6_text.CHALLENGE_6_TEXT
