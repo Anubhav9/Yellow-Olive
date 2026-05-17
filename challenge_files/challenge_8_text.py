c5646939bf84d88b90bb6f09d01902c7b11b1a58
Anubhav Sanyal <princebest3@rediffmail.com>
2026-05-17 11:40:34 +0530

    fixed spaces in challenge text

--- challenge_files/challenge_8_text.py
+++ challenge_files/challenge_8_text.py
@@ -1,17 +1,11 @@
 CHALLENGE_8_TEXT = """Cool Turtle's voice crackles through the tower relay.
-
 "I found Bulba Baby's Pokepod. It's alive. Running. Waiting."
-
 "But every call I send vanishes into static."
 
 Team Evil didn't destroy the pod.
-
 They broke the signal path.
-
 Somewhere in Signal Town, a Service should guide signals to Bulba Baby.
-
 That path is wrong.
-
 Fix the Service so Cool Turtle can reach his friend.
 
 Objective:
