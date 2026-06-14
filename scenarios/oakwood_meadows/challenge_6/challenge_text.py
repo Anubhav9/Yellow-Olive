c0d9a80a708599fe5e0c3e20be06a02bf42bc8f8
Anubhav Sanyal <princebest3@rediffmail.com>
2026-06-14 07:17:17 +0530

    challenge 6 added

--- scenarios/oakwood_meadows/challenge_6/challenge_text.py
+++ scenarios/oakwood_meadows/challenge_6/challenge_text.py
@@ -8,6 +8,7 @@ Too much electricity could damage the Pokepod.
 Control Electromon's energy so that his power stays within safe limits.
 
 Objective:
+Namespace: oakwood-meadows
 Required energy limits:
 cpu = 500m
 memory = 256Mi
